59078a8a02fd5cedf392d2e1c48b7d2571bdcbf0
IlyaB <my.own.securyti@protonmail.com>
2024-04-16 21:25:43 +0300

    SinglePokemon window update

--- Desktop-app/Pokemon/SinglePokemonWindow.axaml.cs
+++ Desktop-app/Pokemon/SinglePokemonWindow.axaml.cs
@@ -21,17 +21,13 @@ public partial class SinglePokemonWindow : Window
         var client = new PokemonApiClient(new HttpClient());
         var pokemon = await client.GetPokemonAsync(StaticData.PokemonId);
         
-        PokemonId.Text = "Id: " + pokemon.PokemonId;
         ImageLoader.SetSource(Image,pokemon.PokemonPhoto);
-        PokemonTitle.Text = "Title: " + pokemon.PokemonTitle;
         PokemonName.Text = "Name: " + pokemon.PokemonName;
-        PokemonType.Text = "Type: " + pokemon.PokemonType;
         PokemonHeight.Text = "Height: " + pokemon.PokemonHeight + " m";
         PokemonWeight.Text = "Weight: " + pokemon.PokemonWeight + " kg";
-        PokemonXpGroup.Text = "XP Group: " + pokemon.PokemonXpGroup;
         PokemonAbilities.Text = "Abilities: " + pokemon.PokemonAbilities;
-        PokemonBreedPeriod.Text = "Breed Period: " + pokemon.PokemonBreedPeriod;
-        PokemonSexRatio.Text = "Sex Ratio: " + pokemon.PokemonSexRatio;
+        PokemonBreedPeriod.Text = "Breed Period: " + pokemon.PokemonBreedPeriod + " d";
+        PokemonSexRatio.Text = pokemon.PokemonSexRatio;
         PokemonTotalAmount.Text = "Total Amount: " + pokemon.PokemonTotalAmount;
         PokemonShine.Text = "Shine: " + (pokemon.PokemonShine ? "Yes" : "No");
         PokemonRarity.Text = "Rarity: " + pokemon.PokemonRarity;
